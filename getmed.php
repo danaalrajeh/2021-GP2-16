a7c6a0673d9ee0da0b22399e5f3534283f0e9bcb
danaalrajeh <78069272+danaalrajeh@users.noreply.github.com>
2022-05-22 22:44:39 +0300

    Delete getmed.php

--- getmed.php
+++
@@ -1,9 +0,0 @@
-<?php
-include ("connection.php");
-if($_SERVER['REQUEST_METHOD'] === 'GET'){//
-    $med_name = $_GET['med_name'];
-    $stmt = $conn->prepare("SELECT descrip FROM medication WHERE med_name = '$med_name'");
-    $stmt->execute();
-    $desc = $stmt->fetch();
-    echo $desc['descrip'];
-}
