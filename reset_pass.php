bf6a6eb4b6a3f8b16d6204e027253e27d8fb8a8f
danaalrajeh <78069272+danaalrajeh@users.noreply.github.com>
2022-05-24 02:41:20 +0300

    Add files via upload

---
+++ reset_pass.php
@@ -0,0 +1,49 @@
+<?php
+ob_start();
+ include 'connection.php';
+
+$email = trim($_POST['email']);
+
+
+$n=15;
+function generatePassword($n) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+  
+    for ($i = 0; $i < $n; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $randomString .= $characters[$index];
+    }
+  
+    return $randomString;
+}
+
+$password = generatePassword($n);
+$password1 = md5($password);
+
+$query = "SELECT * FROM users WHERE email = ? ";
+        $stmt = $conn->prepare($query);
+        $stmt->execute([$email]);
+        $user = $stmt->fetch();
+
+        $count = $stmt->rowCount();
+
+    if($count == 0){
+        header('Location: ' . 'reset_password.php?message=error&email='.$email);
+    }
+else{
+$sql = "update users SET password = ? WHERE email = ?";
+$stmt = $conn->prepare($sql);
+$stmt->execute([$password1,$email]);
+    $to      = $email;
+    $subject = 'Maak account new passowrd';
+    $message = 'The new password is :'. "\r\n" .$password;
+    $headers = 'From: ma3ak2022@gmail.com'       . "\r\n" .
+                 'Your new Password' . "\r\n" .
+                 'by maak';
+                 
+    mail($to, $subject, $message, $headers);
+   echo"goooood";
+   header('Location: ' . 'reset_password.php?message=good&email='.$email);}
+ob_flush();
+?>
